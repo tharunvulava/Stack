5f64be80823f4fb58478d0fcacbb9aefa712cb9e
tharunvulava <90199079+tharunvulava@users.noreply.github.com>
2022-08-15 19:55:03 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	   Stack <Integer> s=new Stack<Integer>(); 
+       s.push(30);                           
+       s.push(9);
+       s.push(2000);
+       s.push(8);
+       s.push(10);
+       s.push(2006);
+       System.out.println(s);
+       System.out.println(s.peek());
+       s.pop();
+       System.out.println(s.peek());
+       s.pop();
+       System.out.println(s);
+	}   
+}
