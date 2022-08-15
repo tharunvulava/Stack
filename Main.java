472d21822de7c902dacc5616f11b4222131bb590
tharunvulava <90199079+tharunvulava@users.noreply.github.com>
2022-08-15 20:00:46 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ public class Main
 	public static void main(String[] args) 
 	{
 	   Stack <Integer> s=new Stack<Integer>(); 
-       s.push(30);                           
-       s.push(9);
-       s.push(2000);
-       s.push(8);
-       s.push(10);
-       s.push(2006);
-       System.out.println(s);
-       System.out.println(s.peek());
-       s.pop();
-       System.out.println(s.peek());
-       s.pop();
-       System.out.println(s);
+           s.push(30);                           
+           s.push(9);
+           s.push(2000);
+           s.push(8);
+           s.push(10);
+           s.push(2006);
+           System.out.println(s);
+           System.out.println(s.peek());
+           s.pop();
+           System.out.println(s.peek());
+           s.pop();
+           System.out.println(s);
 	}   
 }
